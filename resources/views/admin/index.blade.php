9df7661779d457d6c0e27b78a867139de4e6321d
edifalco <estebandifalco@gmail.com>
2016-10-12 16:47:16 +0200

    Users page working without phptos

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,7 @@
 @extends('layouts.admin')
+
+@section('content')
+
+    <h1>This is the admin dashboard.</h1>
+
+@stop
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,35 @@
-hello
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Users</h1>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+          <th>Status</th>
+          <th>Created</th>
+          <th>Updated</th>
+        </tr>
+      </thead>
+      <tbody>
+        @if($users)
+            @foreach($users as $user)
+                <tr>
+                  <td>{{$user->id}}</td>
+                  <td>{{$user->name}}</td>
+                  <td>{{$user->email}}</td>
+                  <th>{{$user->role->name}}</th>
+                  <th>{{$user->is_active == 1 ? 'Active' : 'Not active'}}</th>
+                  <td>{{$user->created_at->diffForHumans()}}</td>
+                  <td>{{$user->updated_at->diffForHumans()}}</td>
+                </tr>
+            @endforeach
+        @endif
+      </tbody>
+    </table>
+
+@stop
